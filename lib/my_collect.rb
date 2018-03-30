28805771b860cc1305592b64139886571f80f027
Joseph Parker <jparker010190@gmail.com>
2018-03-30 00:28:59 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(empty_array)
   i = 0 
   collection = []
   while i < empty_array.length
-  collection << yield(array[i])
+  collection << yield(empty_arrayarray[i])
   i += 1
 end
 collection
